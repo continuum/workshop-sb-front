de34586cf2691ba4941adff22f7179cc3eb48ca9
iescaida <iescaida@iescaidas-MacBook-Pro.local>
2019-10-11 12:15:20 -0300

    Upload spring initializr files

---
+++ src/main/java/cl/continuum/workshop/sb/front/WorkshopSbFrontApplication.java
@@ -0,0 +1,13 @@
+package cl.continuum.workshop.sb.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSbFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSbFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/continuum/workshop/sb/front/WorkshopSbFrontApplicationTests.java
@@ -0,0 +1,16 @@
+package cl.continuum.workshop.sb.front;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopSbFrontApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
